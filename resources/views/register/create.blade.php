a463fa6246d090ecf02dc766d64da3d42b44f2a9
Roy <sssmate@sina.com>
2024-07-23 13:04:53 +0800

    fix password validation

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -50,5 +50,7 @@
 </x-layouts.index>
 
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="/assets/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<script src="/assets/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
 <script src="/assets/pages/scripts/login.min.js" type="text/javascript"></script>
 <!-- END PAGE LEVEL SCRIPTS -->
